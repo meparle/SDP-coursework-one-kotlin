87cf7bbc6444f375c7d075deff18ca1f36ddc808
Eileen Parle <eileenlny@gmail.com>
2018-04-22 21:47:32 +0100

    further tidy of redundant code and kotlin doc

--- src/sml/instructions/OutInstruction.kt
+++ src/sml/instructions/OutInstruction.kt
@@ -3,13 +3,14 @@ package sml.instructions
 import sml.Instruction
 import sml.Machine
 
-class OutInstruction(label: String, val op1: Int): Instruction(label, "out") {
+/**
+ * Describes the SML OUT instruction
+ *
+ */
+class OutInstruction(label: String, val op1: Int): Instruction(label, "Out") {
     override fun execute(m: Machine) {
         val value1 = m.registers.getRegister(op1)
         System.out.println(value1)
     }
 
-    override fun toString(): String {
-        return super.toString()
-    }
 }
